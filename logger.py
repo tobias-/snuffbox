e4e8413d57ad718f9becc8efea6a4c5891948c4d
Tobias <tobias@olsson.be>
2014-12-22 18:26:27 +0100

    New version of logger py

--- logger.py
+++ logger.py
@@ -3,13 +3,15 @@
 import couchdb
 from sys import argv
 import RPi.GPIO as GPIO
-from subprocess import check_output
+from subprocess import Popen, PIPE
 from re import search, M
-from time import time, strftime;
+from time import time, strftime, gmtime, sleep
+import signal
 
 class Snuffbox:
     def updateConfig(self):
         if not self.__monitorName in self.__configdb:
+            self.__config["_id"] = self.__monitorName
             self.__configdb.save(self.__config)
         else:
             cfg = self.__configdb[self.__monitorName]
@@ -18,20 +20,19 @@ class Snuffbox:
                     self.setHeater(False)
                     GPIO.setup(self.config["heater_pin"], GPIO.IN)
                     self.__heater = False
-
-                    self.__config = cfg
+                self.__config = cfg
                 return True
             else:
                 return False
         return True
 
-    def validateConfig(self, cfg = None):
+    def validateConfig(self, cfg=None):
         if not cfg:
             cfg = self.__config
         ok = 1;
-        ok &= str(cfg["poll_interval"]).isdigit()
         ok &= str(cfg["target_temperature"]).isdigit()
         ok &= str(cfg["min_relay_settle"]).isdigit()
+        ok &= str(cfg["min_log_delay"]).isdigit()
         ok &= str(cfg["sensor_pin"]).isdigit()
         ok &= str(cfg["heater_pin"]).isdigit()
         return ok
@@ -45,6 +46,7 @@ class Snuffbox:
         self.__monitorName = name
         self.__heater = 0
         self.__heaterLastChanged = 0
+        self.__logLast = 0
         self.setup()
 
     def setup(self):
@@ -61,9 +63,9 @@ class Snuffbox:
             self.__configdb = couch.create(self.configdb)
         
         self.__config = {
-            "poll_interval": 10,
             "target_temperature": 70,
             "min_relay_settle": 20,
+            "min_log_delay": 60,
             "sensor_pin": 4,
             "heater_pin": 17,
         }
@@ -71,26 +73,44 @@ class Snuffbox:
 
     def setHeater(self, putHeaterOn):
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.config["heater_pin"], GPIO.OUT)
-        GPIO.output(self.config["heater_pin"], putHeaterOn)
+        GPIO.setup(self.__config["heater_pin"], GPIO.OUT)
+        GPIO.output(self.__config["heater_pin"], putHeaterOn)
+        print(strftime("%Y-%m-%d %H:%M:%S: turning heater " + str(putHeaterOn), gmtime()))
 
 
     def loadTempAndHumidity(self):
-        lines = check_output([self.__readTemp, "2302", self.__config["sensor_pin"]])
-        for line in lines.split('\n'):
-            match = search(r'^Temp = +([\d.]+) \*C, Hum = +([\d.]+) %', line, M)
-            if match:
-                result = {}
-                result["actual_temperature"] = match.group(1)
-                result["actual_humidity"] = match.group(2)
-                return result
+        proc = Popen([self.__readTemp, "2302", str(self.__config["sensor_pin"])], stdout=PIPE, stderr=PIPE, bufsize=4096)
+        n = 0;
+        while proc.poll() == None and n < 10:
+            sleep(0.5)
+            n = n + 1
+        if n == 10:
+            proc.kill()
+        else:
+            (lines, foo) = proc.communicate(input)
+            for line in lines.split('\n'):
+                match = search(r'^Temp = +([\d.]+) \*C, Hum = +([\d.]+) %', line, M)
+                if match:
+                    result = {}
+                    result["actual_temperature"] = match.group(1)
+                    result["actual_humidity"] = match.group(2)
+                    print("Temperature: " + match.group(1) + " Humidity: " + match.group(2))
+                    return result
         return None
 
-
-    def __upload(self, result):
-        doc = result.clone()
-        doc["unix_time"] = int(time.time())
-        doc["current_time"] = strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+    def __upload(self, result, id = None):
+        print(strftime("%Y-%m-%d %H:%M:%S: Logging", gmtime()))
+        if id:
+            try:
+                doc = self.__couchdb[id]
+                doc = dict(doc.items() + result.items())
+            except:
+                doc = result.copy()
+                doc["_id"] = id
+        else:
+            doc = result.copy()
+        doc["unix_time"] = int(time())
+        doc["current_time"] = strftime("%Y-%m-%d %H:%M:%S", gmtime())
         if self.__heater:
             doc["heater_on"] = "true"
         else:
@@ -98,29 +118,40 @@ class Snuffbox:
         doc["sensor_pin"] = self.__config["sensor_pin"]
         doc["target_temperature"] = self.__config["target_temperature"]
         doc["config_name"] = self.__monitorName
+        print(doc)
         self.__couchdb.save(doc);
-
 
     def run(self):
         self.updateConfig()
         if self.validateConfig():
             measured = self.loadTempAndHumidity()
-            if measured and self.__heaterLastChanged < (time.time() - self.__config["min_relay_settle"]):
-                wantedheat = False
-                if self.__config["target_temperature"] < measured["actual_temperature"]:
-                    wantedheat = True
-                if wantedheat != self.__heater:
-                    self.setHeater(wantedheat)
-                    self.__heaterLastChanged = time.time()
-                    self.__heater = wantedheat
+            if measured:
+                if self.__heaterLastChanged < (time() - self.__config["min_relay_settle"]):
+                    wantedheat = False
+                    if float(self.__config["target_temperature"]) > float(measured["actual_temperature"]):
+                        wantedheat = True
+                    if wantedheat != self.__heater:
+                        self.setHeater(wantedheat)
+                        self.__heaterLastChanged = time()
+                        self.__heater = wantedheat
+                if self.__logLast < (time() - self.__config["min_log_delay"]):
+                    self.__upload(measured)
+                    self.__logLast = time()
+                self.__upload(measured, self.__monitorName+":current_temp")
+
+
+    def shutdown(self, signum, stack):
+        print("Shutting down")
+        self.setHeater(False)
 
 
 if __name__ == "__main__":
     name = "local"
     if len(argv) > 1:
         name = argv[1]
-    foo = Snuffbox(name)
-    while True:
-        print("Running")
-        foo.run()
-
+    snuffbox = Snuffbox(name)
+    try:
+        while True:
+            snuffbox.run()
+    finally:
+        snuffbox.shutdown("", "")
